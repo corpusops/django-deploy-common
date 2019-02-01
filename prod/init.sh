fe95a0261650b532756fa8657bb204858f912c31
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-02-01 23:50:20 +0100

    use rsyslog

--- prod/init.sh
+++ prod/init.sh
@@ -290,7 +290,7 @@ if [[ -z "$@" ]]; then
             log "Missing: $cfg"
             exit 1
         fi
-        SUPERVISORD_CONFIGS="$cfg" exec /bin/supervisord.sh
+        SUPERVISORD_CONFIGS="/etc/supervisor.d/rsyslog $cfg" exec /bin/supervisord.sh
     fi
 else
     if [[ "${1-}" = "shell" ]];then shift;fi
